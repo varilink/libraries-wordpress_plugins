f49edbd0f1e8cc4e3f4be8a884c6eee82781aa6f
David Williamson <david.williamson@varilink.co.uk>
2023-09-16 18:55:33 +0100

    TRIM SOME EXCESSIVE WHITESPACE IN THE MAILCHIMP PLUGIN'S MAIN FILE

--- varilink-mailchimp/varilink-mailchimp.php
+++ varilink-mailchimp/varilink-mailchimp.php
@@ -8,68 +8,68 @@
  */
 
 // Protect from being called outside of WordPress
-defined ( 'ABSPATH' ) or die ( 'Access Denied' ) ;
+defined( 'ABSPATH' ) or die( 'Access Denied' );
 
-function varilink_mailchimp_information_about_a_list_member (
-  $api_key , $api_root , $list_id , $email , $fields
+function varilink_mailchimp_information_about_a_list_member(
+  $api_key, $api_root, $list_id, $email, $fields
 ) {
 
-  if ( function_exists ( 'varilink_write_log' ) ) {
-    varilink_write_log (
+  if ( function_exists( 'varilink_write_log' ) ) {
+    varilink_write_log(
       'function varilink_mailchimp_information_about_a_list_member called'
-    ) ;
+    );
   }
 
-  $subscriber_hash = md5 ( strtolower ( $email ) ) ;
+  $subscriber_hash = md5( strtolower( $email ) );
 
-  $ch = curl_init ( ) ;
-  curl_setopt (
-    $ch ,
-    CURLOPT_URL ,
+  $ch = curl_init();
+  curl_setopt(
+    $ch,
+    CURLOPT_URL,
     "https://$api_root/lists/$list_id/members/$subscriber_hash?fields=$fields"
-  ) ;
-  curl_setopt ( $ch , CURLOPT_RETURNTRANSFER , TRUE ) ;
-  curl_setopt ( $ch , CURLOPT_USERPWD , "anystring:$api_key") ;
-  $body = curl_exec ( $ch ) ;
-  $rc = curl_getinfo ( $ch , CURLINFO_HTTP_CODE ) ;
+  );
+  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, TRUE );
+  curl_setopt( $ch, CURLOPT_USERPWD, "anystring:$api_key");
+  $body = curl_exec( $ch );
+  $rc = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
   if ( $rc === 200 ) {
-    $response = json_decode ( $body ) ;
+    $response = json_decode( $body );
   } else {
-    $response = FALSE ;
+    $response = FALSE;
   }
-  curl_close ( $ch ) ;
+  curl_close( $ch );
 
-  return $response ;
+  return $response;
 
 }
 
-function varilink_mailchimp_add_or_update_list_member (
-  $api_key , $api_root , $list_id , $request
+function varilink_mailchimp_add_or_update_list_member(
+  $api_key, $api_root, $list_id, $request
 ) {
 
-  $subscriber_hash = md5 ( strtolower ( $request [ 'email_address' ] ) ) ;
+  $subscriber_hash = md5( strtolower( $request [ 'email_address' ] ) );
 
-  $ch = curl_init ( ) ;
-  curl_setopt (
-    $ch ,
-    CURLOPT_URL ,
+  $ch = curl_init();
+  curl_setopt(
+    $ch,
+    CURLOPT_URL,
     "https://$api_root/lists/$list_id/members/$subscriber_hash"
-  ) ;
-  curl_setopt ( $ch , CURLOPT_CUSTOMREQUEST , 'PUT' ) ;
-  curl_setopt ( $ch , CURLOPT_RETURNTRANSFER , TRUE ) ;
-  curl_setopt ( $ch , CURLOPT_USERPWD , "anystring:$api_key") ;
-  $json = json_encode ( $request ) ;
-  curl_setopt ( $ch , CURLOPT_POSTFIELDS , $json ) ;
-  curl_setopt (
-    $ch ,
-    CURLOPT_HTTPHEADER ,
-    array ( 'Content-Type: application/json' )
-  ) ;
-  $body = curl_exec ( $ch ) ;
-  $rc = curl_getinfo ( $ch , CURLINFO_HTTP_CODE ) ;
-  $response = json_decode ( $body ) ;
-  curl_close ( $ch ) ;
+  );
+  curl_setopt( $ch, CURLOPT_CUSTOMREQUEST , 'PUT' );
+  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, TRUE );
+  curl_setopt( $ch, CURLOPT_USERPWD, "anystring:$api_key");
+  $json = json_encode( $request );
+  curl_setopt( $ch, CURLOPT_POSTFIELDS, $json );
+  curl_setopt(
+    $ch,
+    CURLOPT_HTTPHEADER,
+    array( 'Content-Type: application/json' )
+  );
+  $body = curl_exec( $ch );
+  $rc = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+  $response = json_decode( $body );
+  curl_close( $ch );
 
-  return $response ;
+  return $response;
 
 }
